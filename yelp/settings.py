2eccfd68559eb3f6d6f28b2f1c800737c7fe5513
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-05-01 19:30:16 -0700

    Added bookmark application to project and created first view.

--- yelp/settings.py
+++ yelp/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookmark.apps.BookmarkConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
