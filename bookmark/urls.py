e340d640c20814408916d19e9899188ae4a65a3d
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-05-07 23:07:18 -0700

    Added /bookmark to views.

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='bookmark-home')
+    path('', views.home, name='bookmark-home'),
+    path('bookmarks', views.bookmarks, name='bookmark-bookmarks')
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
-from bookmark import restaurants
+from bookmark.api import bookmarks as bm, restaurants
 
 
 def home(request):
     return render(request, 'bookmark/home.html', {'title': 'Home Page', 'restaurants': restaurants.RESTAURANTS})
+
+
+def bookmarks(request):
+    return render(request, 'bookmark/bookmarks.html', {'title': 'Bookmarks', 'restaurants': bm.RESTAURANTS})
