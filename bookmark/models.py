2b2fb7e2da744d600b844eac1b5ce461d0b95af4
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-05-07 23:20:29 -0700

    Added Restaurant model.

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    business_id = models.CharField(max_length=100)
+    visited = models.BooleanField()
+    bookmark = models.BooleanField()
+
+    def __str__(self):
+        return self.name
