e6bc96e791329d9b869fa023bc77735c57d9987d
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-05-13 12:51:02 -0700

    Added event handler for visited button.

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
+from django.conf.urls import url
+from bookmark import views
 
 urlpatterns = [
     path('', views.home, name='bookmark-home'),
     path('bookmarks', views.bookmarks, name='bookmark-bookmarks'),
-    path('visited', views.visited, name='bookmark-visited')
+    path('visited', views.visited, name='bookmark-visited'),
+    url(r'^', views.add_to_visited, name='add-to-visited')
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core import exceptions
+from django.shortcuts import render, redirect
 
 from bookmark.api import bookmarks as bm, restaurants, visited as vis
+from bookmark.forms import VisitedForm
+from bookmark.models import Restaurant
 
 
 def home(request):
@@ -13,3 +17,42 @@ def bookmarks(request):
 
 def visited(request):
     return render(request, 'bookmark/visited.html', {'title': 'Visited', 'restaurants': vis.RESTAURANTS})
+
+
+def add_to_visited(request):
+    # If this is a POST request we need to process the form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request:
+        form = VisitedForm(request.POST)
+
+        # Check whether it's valid:
+        if form.is_valid():
+            # Save name and business_id
+            name = form.cleaned_data.get('name')
+            business_id = form.cleaned_data.get('business_id')
+
+            # Attempt retrieval of object from database
+            try:
+                r = Restaurant.objects.filter(business_id=business_id).get()
+                if r.visited:
+                    # Object is already in visited, display warning message
+                    messages.warning(request, f'{name} is already in your visited list!')
+                    return redirect('bookmark-home')
+                else:
+                    # Object is in bookmarks, move to visited and remove from bookmarks
+                    r.visited = True
+                    r.bookmark = False
+                    r.save()
+            except exceptions.ObjectDoesNotExist:
+                # Object does not yet exist, save to database
+                form.save()
+
+            # Display success message
+            messages.success(request, f'Added {name} to visited!')
+        else:
+            # Form is invalid (i.e. missing fields), display error message
+            messages.error(request, 'An error occurred! Please try again later.', extra_tags='danger')
+
+        # Redirect user to the previous page and display alerts
+        return redirect(request.META.get('HTTP_REFERER'))
