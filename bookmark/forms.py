4116540f083701dd0cf5372da6a6c0ef36ebdb8c
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-05-20 13:00:08 -0700

    Removed name and address from Restaurant model.

--- bookmark/forms.py
+++ bookmark/forms.py
@@ -4,18 +4,24 @@ from bookmark.models import Restaurant
 
 
 class VisitedForm(forms.ModelForm):
+    name = forms.CharField(max_length=100)
+
     class Meta:
         model = Restaurant
         fields = "__all__"
 
 
 class BookmarkForm(forms.ModelForm):
+    name = forms.CharField(max_length=100)
+
     class Meta:
         model = Restaurant
         fields = "__all__"
 
 
 class DeleteForm(forms.ModelForm):
+    name = forms.CharField(max_length=100)
+
     class Meta:
         model = Restaurant
-        fields = {'name', 'business_id'}
+        fields = {'business_id'}
--- bookmark/models.py
+++ bookmark/models.py
@@ -2,11 +2,6 @@ from django.db import models
 
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
     business_id = models.CharField(max_length=100)
     visited = models.BooleanField()
     bookmark = models.BooleanField()
-
-    def __str__(self):
-        return self.name
