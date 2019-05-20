28d48649aa8cbc2d1ff4653b159a0b8b846e04cb
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-05-20 16:43:26 -0700

    Added event handler for date-added button.

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     url(r'^add-to-bookmarks', views.bookmarks.AddToBookmarksView.as_view(), name='add-to-bookmarks'),
     url(r'^add-to-visited', views.visited.AddToVisitedView.as_view(), name='add-to-visited'),
     url(r'^delete', views.DeleteView.as_view(), name='delete'),
-    url(r'^bookmarks/is-open-now', views.IsOpenNow.as_view(), name='is-open-now'),
-    url(r'^bookmarks/sort-by-cuisine', views.SortByCuisine.as_view(), name='sort-by-cuisine'),
-    url(r'^bookmarks/sort-by-rating', views.SortByRating.as_view(), name='sort-by-rating'),
+    url(r'^bookmarks/sort-by-distance', views.SortByDistance.as_view(), name='sort-by-distance'),
     url(r'^bookmarks/sort-by-popularity', views.SortByPopularity.as_view(), name='sort-by-popularity'),
-    url(r'^bookmarks/sort-by-distance', views.SortByDistance.as_view(), name='sort-by-distance')
+    url(r'^bookmarks/sort-by-rating', views.SortByRating.as_view(), name='sort-by-rating'),
+    url(r'^bookmarks/sort-by-cuisine', views.SortByCuisine.as_view(), name='sort-by-cuisine'),
+    url(r'^bookmarks/is-open-now', views.IsOpenNow.as_view(), name='is-open-now')
 ]
