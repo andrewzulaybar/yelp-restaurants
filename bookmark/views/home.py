cb80d2861845d728c040aaa550c329c139c1d1ba
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-06-28 11:32:44 -0700

    Added pagination for home page.

--- bookmark/views/home.py
+++ bookmark/views/home.py
@@ -13,11 +13,20 @@ class HomeListView(ListView):
         context = super(HomeListView, self).get_context_data(**kwargs)
         context['title'] = 'Home Page'
 
+        # Retrieve page number from URL
+        page_num = 1
+        if self.request.GET.get('page'):
+            page_num = int(self.request.GET.get('page'))
+
         # Additional parameters
-        params = {'location': location.CURRENT_LOCATION, 'sort_by': 'distance', 'categories': 'restaurants'}
+        params = {'location': location.CURRENT_LOCATION,
+                  'sort_by': 'distance',
+                  'categories': 'restaurants',
+                  'offset': page_num * 20}
 
         # Make Yelp Fusion API GET request for nearby restaurants
         restaurants = yelp_api.get("v3/businesses/search", params)
 
-        context['restaurants'] = restaurants['businesses']
+        context[self.context_object_name] = restaurants['businesses']
+        context['page'] = page_num
         return context
