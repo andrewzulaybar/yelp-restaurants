e08c7f56a69c74941c231219582814900ee0349e
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-06-28 19:56:39 -0700

    Added pagination to visited.

--- bookmark/views/visited.py
+++ bookmark/views/visited.py
@@ -4,56 +4,30 @@ from bookmark.models import *
 
 
 class VisitedListView(ListView):
-    model = Restaurant
+    model = Visited
     template_name = 'bookmark/visited.html'
     context_object_name = 'restaurants'
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super(VisitedListView, self).get_context_data(**kwargs)
         return self.get_visited(context)
 
-    def get_visited(self, context):
+    @staticmethod
+    def get_visited(context):
         context['title'] = 'Visited'
 
-        # Retrieve visited objects
-        restaurants = Visited.objects.raw(
-            ''' SELECT * 
-                FROM bookmark_Visited v
-                INNER JOIN bookmark_Restaurant r on v.restaurant_id = r.business_id
-                INNER JOIN bookmark_Location l on r.location_id = l.id '''
-        )
+        # Retrieve restaurants for each in visited
+        restaurants = []
+        for visited in context['restaurants']:
+            restaurants.append(visited.restaurant_id)
+        context['restaurants'] = Restaurant.objects.filter(business_id__in=restaurants)
 
-        # Format restaurants for template
-        visited = []
-        for restaurant in restaurants:
-            # Retrieve categories for restaurant
-            categories = []
-            all_categories = Category.objects.raw(
-                ''' SELECT *
-                    FROM bookmark_Restaurant r
-                    INNER JOIN bookmark_RestaurantHasCategory rhc ON r.business_id = rhc.restaurant_id
-                    INNER JOIN bookmark_Category c ON rhc.category_id = c.title
-                    WHERE r.business_id = '%s' ''' % restaurant.business_id
-            )
-            for category in all_categories:
-                categories.append(category.title)
-
-            # Setup context for template
-            res = {
-                'business_id': restaurant.business_id,
-                'name': restaurant.name,
-                'rating': restaurant.rating,
-                'review_count': restaurant.review_count,
-                'price': restaurant.price,
-                'phone': restaurant.phone,
-                'image_url': restaurant.image_url,
-                'yelp_url': restaurant.yelp_url,
-                'location_id': restaurant.location_id,
-                'address': restaurant.address,
-                'categories': categories
-            }
-            visited.append(res)
-
-        context[self.context_object_name] = visited
+        # Retrieve categories for each restaurant
+        categories = []
+        for restaurant in context['restaurants']:
+            category = RestaurantHasCategory.objects.filter(restaurant=restaurant)
+            categories.append(category[0])
+        context['categories'] = categories
         return context
 
